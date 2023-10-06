29540706550b7a9c68e2647ed19151a96f55c727
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-10-06 12:25:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,18 +6,28 @@ let elements = []; // Array to hold generated DOM elements
 // Function to generate DOM elements
 const generateElements = () => {
   // complete the function
+	const newElement = document.createElement('p');
+  newElement.textContent = 'New Element';
+  document.body.appendChild(newElement);
+  elements.push(newElement);
   updateMemoryUsage();
 };
 
 // Function to remove DOM elements
 const removeElements = () => {
   // complete the function
+	if (elements.length > 0) {
+    const elementToRemove = elements.pop();
+    document.body.removeChild(elementToRemove);
+  }
   updateMemoryUsage();
 };
 
 // Function to update memory usage display
 const updateMemoryUsage = () => {
   // Complete this function
+	const memoryUsedInMB = window.performance.memory.usedJSHeapSize / (1024 * 1024);
+  document.getElementById('memory').textContent = `Memory used: ${memoryUsedInMB.toFixed(2)} MB`;
 };
 
 // Add event listeners to buttons
